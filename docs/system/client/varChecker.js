a3910c4c54a2ba2ee48a5f44165a9a111c8b9fd2
TheUnchosenOne <63002752+TheUnch0senOne@users.noreply.github.com>
2020-05-28 22:00:43 +0800

    Create varChecker.js

---
+++ docs/system/client/varChecker.js
@@ -0,0 +1,13 @@
+window.varChecker = {};
+varChecker.output = {};
+varChecker.function = (var, todo) => {
+function
+Object.defineProperty(this, variable, {
+  get: function () { return myVar; },
+  set: function (v) {
+    (() => {return variable;})() = v;
+    
+  }
+});
+return
+}
